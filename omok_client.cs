6746d021b24c74881761098de8814d66bfd92875
olambdao <olambdao@gmail.com>
2016-11-15 16:06:45 +0900

    omok_client, omok_your_client c# 버전 추가함

---
+++ omok_client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmokClientExample
+{
+    public class OmokClient
+    {
+        const int EYE_OFFSET = 3;
+
+        public int boardSize;
+        public int[,] board;
+
+        public int myId;
+
+        public OmokClient(int boardSize)
+        {
+            this.boardSize = boardSize;
+            this.board = new int[boardSize, boardSize];
+        }
+
+        string _get()
+        {
+            // 호스트와 통신
+            return Console.ReadLine().Trim();
+        }
+
+        void _put(string text)
+        {
+            // 호스트와 통신
+            Console.WriteLine(text);
+            Console.Out.Flush();
+        }
+
+        public Tuple<int, int, int> get()
+        {
+            // 호스트에게 상태와 상대의 좌표를 가져온다.
+            var tmp = _get();
+            if (tmp == "quit")
+                return Tuple.Create(-1, -1, -1);
+
+            var xy = tmp.Split();
+            var x = int.Parse(xy[0]);
+            var y = int.Parse(xy[1]);
+            if (x != -1 || y != -1)
+                board[y, x] = EYE_OFFSET - myId;
+
+            return Tuple.Create(1, x, y);
+        }
+
+        public void put(int x, int y)
+        {
+            board[y, x] = myId;
+            // 나의 수(좌표)를 호스트에게 알려준다.
+            _put(string.Format("{0} {1}", x, y));
+        }
+
+        public void ready()
+        {
+            // 호스트로부터 아이디를 가져옵니다.
+            myId = int.Parse(_get());
+        }
+    }
+}
---
+++ omok_your_client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmokClientExample
+{
+    class Program
+    {
+        const int BOARD_SIZE = 15;
+
+        static Tuple<int, int> Choose(int myId, int px, int py, int[,] board)
+        {
+            // my_id: 나의 식별자 (흑:1, 백: 2)
+            // px, py: 방금전 상대편이 둔 수 (주의!맨 처음 수는 - 1, -1로 주어진다)
+            // board: 현재 상태 수
+
+            var boardSize = board.Length;
+            for (var y = 0; y < boardSize; ++y)
+                for (var x = 0; y < boardSize; ++x)
+                    if (board[y, x] == 0)
+                        return Tuple.Create(x, y);
+
+            return Tuple.Create(-1, -1);
+        }
+
+        static void Main(string[] args)
+        {
+            var game = new OmokClient(BOARD_SIZE);
+            game.ready();
+
+            while (true)
+            {
+                // sync
+                var tmp = game.get();
+                if (tmp.Item1 == -1)
+                    break;
+
+                var x = tmp.Item2;
+                var y = tmp.Item3;
+
+                // 자신의 수 선택
+                var choose = Choose(game.myId, x, y, game.board);
+                x = choose.Item1;
+                y = choose.Item2;
+
+                // sync
+                game.put(x, y);
+            }
+        }
+    }
+}
