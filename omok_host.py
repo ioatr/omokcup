5e26ada33b2ff6b67d3fbe80f3e157dc6e5e0bac
olambdao S LIM <olambdao@gmail.com>
2016-11-24 16:31:17 +0900

    omok_host.py 버그 수정, 리팩토링하면서 오타난거 수정

--- omok_host.py
+++ omok_host.py
@@ -226,8 +226,8 @@ def do(args1, args2):
 
 		except OmokException as e:
 			# 플레이어가 예외상황(타임아웃)이라면, 상대 플레이어 승으로 간주한다
-			winner = players[EYE_OFFSET - e.index]
-			print set_color('r', '{} IS TIMEOUT'.format(e.getname()))
+			winner = players[EYE_OFFSET - e.player.index]
+			print set_color('r', '{} IS TIMEOUT'.format(e.player.getname()))
 
 		except:
 			import traceback
